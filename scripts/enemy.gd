cb2464385b60abb4ae446d7dc3f4147ffbe291b6
michael-a-jung <152505444+michael-a-jung@users.noreply.github.com>
2025-09-24 20:47:56 +0200

    revert: enemy scene

--- scripts/enemy.gd
+++
@@ -1,9 +0,0 @@
-extends RigidBody2D
-
-@export var speed: int = 200
-
-func _process(delta: float) -> void:
-	position += Vector2(100, 0) * delta
-
-func _on_visible_on_screen_notifier_2d_screen_exited():
-	queue_free()

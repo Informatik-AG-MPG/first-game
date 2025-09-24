286632089607bc83ef46d5c0f8dcb1df728f8fb8
michael-a-jung <152505444+michael-a-jung@users.noreply.github.com>
2025-09-24 20:46:27 +0200

    feat: add Info-AG-Logo as sprite

--- scripts/player.gd
+++ scripts/player.gd
@@ -11,8 +11,8 @@ func _init():
 
 func _process(delta: float) -> void:
 	# check for user input
-	if Input.is_action_pressed("right"):
-		position += Vector2(speed, 0) * delta
+	if Input.is_action_pressed("right") == true:
+		position = position + Vector2(speed, 0) * delta
 	if Input.is_action_pressed("left"):
 		position += Vector2(-speed, 0) * delta
 	if Input.is_action_pressed("up"):
